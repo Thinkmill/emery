17b54827092c8a735116268eb3be7e922041a3e0
jossmac <jossmac@users.noreply.github.com>
2022-05-17 14:01:23 +1000

    update `typeInvariant()` description

--- src/invariant/typeInvariant.ts
+++ src/invariant/typeInvariant.ts
@@ -1,13 +1,13 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 /**
- * An `invariant` function takes a condition:
- * - when "falsy" the function will throw
- * - when "truthy" the function will NOT throw
+ * Asserts the "truthiness" of a condition:
+ * - will throw when ["falsy"](https://developer.mozilla.org/en-US/docs/Glossary/Falsy)
+ * - will NOT throw when ["truthy"](https://developer.mozilla.org/en-US/docs/Glossary/Truthy)
  *
- * While TypeScript is expressive, the errors it returns can sometimes be
- * difficult to decipher, especially for folks who are still learning. Runtime
- * type errors are expected to complement type definitions, not replace them.
+ * While TypeScript is expressive, the errors it returns can be difficult to
+ * decipher, especially for folks who are still learning. Runtime type errors
+ * are expected to complement type definitions, not replace them.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function typeInvariant(condition: any, message: string): asserts condition {
