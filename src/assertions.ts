9205bc2b782238ed5bf0d42a487e2f95ee8c9846
jossmac <jossmac@users.noreply.github.com>
2022-07-07 08:43:32 +1000

    assertions: support opt-out of debugger

--- src/assertions.ts
+++ src/assertions.ts
@@ -9,9 +9,13 @@
 // intentional design decision. The goal is to promote consideration from
 // consumers when dealing with potentially ambiguous conditions like `0` or
 // `''`, which can introduce "subtle" bugs.
-export function assert(condition: boolean, message = 'Assert failed'): asserts condition {
+export function assert(
+  condition: boolean,
+  message = 'Assert failed',
+  options = { debug: true },
+): asserts condition {
   if (!condition) {
-    developmentDebugger();
+    developmentDebugger(options.debug);
     throw new TypeError(message);
   }
 }
@@ -22,14 +26,14 @@ export function assert(condition: boolean, message = 'Assert failed'): asserts c
  * @throws always
  * @returns void
  */
-export function assertNever(condition: never): never {
-  developmentDebugger();
+export function assertNever(condition: never, options = { debug: true }): never {
+  developmentDebugger(options.debug);
   throw new Error(`Unexpected call to assertNever: '${condition}'`);
 }
 
 /** Pause execution in development to aid debugging. */
-function developmentDebugger() {
-  if (process.env.NODE_ENV === 'production') {
+function developmentDebugger(enabled?: boolean): void {
+  if (!enabled || process.env.NODE_ENV === 'production') {
     return;
   }
 
